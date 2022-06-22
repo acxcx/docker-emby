4841ff9e5adcb96a2ca1af00d424d1cb860a7dc5
Alexander Morozov <9192541+potatoru@users.noreply.github.com>
2022-06-22 17:04:57 +1100

    Update patch.sh

--- patch/patch.sh
+++ patch/patch.sh
@@ -1,1 +1,21 @@
+#!/bin/sh
 
+chmod +x /patch/ilasm
+chmod +x /patch/ildasm
+mkdir /patch/tmp && cd /patch/tmp
+
+# Patch web client
+/patch/ildasm /app/emby/Emby.Web.dll -out=Emby.Web.dll
+sed -i 's/mb3admin.com/tsumo.cf/g' Emby.Web.dashboard_ui.modules.emby_apiclient.connectionmanager.js
+/patch/ilasm -dll Emby.Web.dll -out=/app/emby/Emby.Web.dll
+rm Emby.Web.*
+
+sed -i 's/mb3admin.com/tsumo.cf/g' /app/emby/dashboard-ui/embypremiere/embypremiere.js
+
+# Patch internal
+/patch/ildasm /app/emby/Emby.Server.Implementations.dll -out=Emby.Server.Implementations.dll
+sed -i 's/\/mb3admin.com/\/tsumo.cf/g' Emby.Server.Implementations.dll
+/patch/ilasm -dll Emby.Server.Implementations.dll -out=/app/emby/Emby.Server.Implementations.dll
+
+# Delete tmp
+cd ../ && rm -rf tmp
